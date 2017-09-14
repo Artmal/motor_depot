5b7d6ba9661fb53f4fbdb021e3e313496774356e
Artem Malchenko <artmalchik@gmail.com>
2017-09-14 12:50:45 +0300

    Security filter refactored.

--- src/main/java/com/artmal/utils/ValidationException.java
+++ src/main/java/com/artmal/utils/ValidationException.java
@@ -1,5 +1,9 @@
 package com.artmal.utils;
 
+/**
+ * Exception for server-side validation.
+ * @author Artem Malchenko
+ */
 public class ValidationException extends Exception {
     public ValidationException() {
         super();
